4e6f8a70433c8e4616c30917a260939f541ad1b9
Václav Fišer <fiserva3@fel.cvut.cz>
2025-04-19 14:12:50 +0200

    Added movement to enemies

--- scripts/enemy_skeleton_1.gd
+++ scripts/enemy_skeleton_1.gd
@@ -7,12 +7,15 @@ const Player = preload("res://scripts/player_simple.gd")
 const AttackDTO = preload("res://scripts/attack_dto.gd")
 const HealthComponent = preload("res://scripts/health_component.gd")
 
-const SPEED = 300.0
+const SPEED = 3000.0
 const JUMP_VELOCITY = -400.0
 const DAMAGE = 10
 const MAX_HEALTH = 40
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var navigation_agent := $NavigationAgent2D as NavigationAgent2D
+var target : Node2D
+var player_detected = false
 
 var health_component
 
@@ -25,17 +28,24 @@ func _ready() -> void:
 	self.add_child(health_component)
 	health_component.connect("health_depleted", _on_health_component_health_depleted)
 	health_component.connect
+	player_detected = false
 	
 
 func _physics_process(delta: float) -> void:
 	match state:
 		STATE.IDLE:
 			animated_sprite.play("idle")
+			velocity = Vector2(0.0, 0.0)
 		STATE.FOLLOWING:
 			animated_sprite.play("move")
+			var move_dir = to_local(navigation_agent.get_next_path_position()).normalized()
+			velocity = move_dir * SPEED  * delta
+			move_and_slide()
 		STATE.ATTACKING:
 			animated_sprite.play("attack")
+			velocity = Vector2(0.0, 0.0)
 		STATE.DEAD:
+			velocity = Vector2(0.0, 0.0)
 			return
 
 func incomming_attack(attack_dto: AttackDTO):
@@ -48,17 +58,27 @@ func attack(player: Player):
 	player.incomming_attack(attack_dto)
 
 
+
+func _on_pathfinding_timer_timeout() -> void:
+	if player_detected:
+		navigation_agent.target_position = target.global_position
+
 func _on_detection_range_body_entered(body: Node2D) -> void:
 	# If player -> start following
 	#print("BDG: player entered detection range")
-	# state = STATE.FOLLOWING
+	player_detected = true
+	state = STATE.FOLLOWING
+	target = body
+	navigation_agent.target_position = body.global_position
+	print(body.global_position)
 	pass
 
 
 func _on_detection_range_body_exited(body: Node2D) -> void:
 	# If player and following -> stop following
 	#print("BDG: player exited detection range")
-	# state = STATE.IDLE
+	state = STATE.IDLE
+	player_detected = false
 	pass
 
 
